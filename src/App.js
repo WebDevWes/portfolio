6acc64867f39368a67084fc764f9f18d8b5b8c81
Wesley <wesaboo@gmail.com>
2020-09-29 20:01:21 -0700

    adds react-rnd, window component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
+import WindowContainer from "./components/WindowContainer";
+import { Rnd } from "react-rnd";
 
 function App() {
   return (
-    <div className="App Background" style={{ backgroundColor: "#66a", minHeight:"100vh", height: "100%" }}>
+    <div
+      style={{ backgroundColor: "#66a", minHeight: "100vh", height: "100%" }}
+    >
       <Navbar />
+      <Rnd>
+        <WindowContainer />
+      </Rnd>
       <Footer />
     </div>
   );
